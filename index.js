f92b6954ac3d0b29ea54d4fcf6cbffe1b4e03152
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2022-06-06 12:04:58 +0200

    fixed channel type

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ bot.commands = [
 				option.setName('channel')
 					.setDescription('The channel id')
 					.setRequired(true)
-					.addChannelTypes([ChannelType.GuildText,ChannelType.GuildNews]))
+					.addChannelType(ChannelType.GuildText)
+					.addChannelType(ChannelType.GuildNews))
 			.addRoleOption(option =>
 				option.setName("ping")
 				.setDescription("The role to ping when a new update is released")
